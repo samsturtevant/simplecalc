b048d554182174a27cec6f429a11d4d63984e701
Sam Sturtevant <samstur@uw.edu>
2018-01-11 19:34:42 -0800

    Finished simplecalc assignment

--- SimpleCalc/SimpleCalc/main.swift
+++ SimpleCalc/SimpleCalc/main.swift
@@ -10,6 +10,55 @@ import Foundation
 
 public class Calculator {
     public func calculate(_ args: [String]) -> Int {
+        if args.contains("count") {
+            var count = 0
+            for arg in args {
+                if Int(arg) != nil {
+                    count += 1
+                }
+            }
+            return count
+        } else if args.contains("avg") {
+            var sum = 0
+            var count = 0
+            for arg in args {
+                if Int(arg) != nil {
+                    count += 1
+                    sum += Int(arg)!
+                }
+            }
+            if count > 0 {
+                return sum / count
+            } else {
+                return 0
+            }
+        } else if args.contains("fact") {
+            var fact = 1
+            for arg in args {
+                if Int(arg) != nil {
+                    var num = Int(arg)!
+                    while num > 0 {
+                        fact = fact * num
+                        num -= 1
+                    }
+                    return fact
+                }
+            }
+            return 0
+        } else {
+            switch args[1]{
+            case "+":
+                return Int(args[0])! + Int(args[2])!
+            case "-":
+                return Int(args[0])! - Int(args[2])!
+            case "/":
+                return Int(args[0])! / Int(args[2])!
+            case "*":
+                return Int(args[0])! * Int(args[2])!
+            default:
+                return 0
+            }
+        }
     }
     
     public func calculate(_ arg: String) -> Int {
